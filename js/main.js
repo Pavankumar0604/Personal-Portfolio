285414bd78092498348471f3a33996fe17dfc8ea
Penumuru Pavan Kumar <113255655+Pavankumar0604@users.noreply.github.com>
2025-06-22 15:41:02 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const toggle = document.getElementById("nav-toggle");
+  const nav = document.getElementById("nav-menu");
+
+  if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+      nav.classList.toggle("show");
+    });
+  }
+});
